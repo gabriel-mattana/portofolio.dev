2332336d09d38bd0020883cdd95794bdc29e9a10
gabLinux <thekrelcompany@gmail.com>
2023-01-07 12:10:14 +0100

    create a signleton of ContentDisplayer and update its state by cliking navButton

--- src/components/content/ContentDisplayer.jsx
+++ src/components/content/ContentDisplayer.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ContentDisplayer({content})
-{
-    return(
-        <main>{content}</main>
-    );
+
+let ContentDisplayerInstance;
+
+class ContentDisplayer extends React.Component {
+
+    constructor()
+    {
+        super();
+        console.log("Content displayer created");
+        ContentDisplayerInstance = ContentDisplayerInstance || this; 
+        this.state = {content:<h1>Empty</h1>}
+    }
+
+    UpdateContent(props)
+    {
+        this.setState({content: <h1>{props}</h1>});
+    }
+
+   render() {
+
+    return <main id="content-displayer">Content Displayer 
+            {this.state.content}
+            </main>;
+  }
 }
 
+export {ContentDisplayerInstance};
 export default ContentDisplayer;
--- src/components/navbar/NavButton.jsx
+++ src/components/navbar/NavButton.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import {ContentDisplayerInstance} from "../content/ContentDisplayer";
 
 function NavButton(props) 
 {
 
   function UpdateContent() {
+    
     console.log("Clicked on " + props.label);
+    var displayer = ContentDisplayerInstance;
+    console.log(displayer);
+    displayer.UpdateContent("hahaha");
   }
 
   console.log("NavItem " + props.label + " rendered");
