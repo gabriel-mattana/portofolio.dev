390168b056238d6c4d066b3f8b9f7f7eb0d2de9e
Gabriel <gabriel.mattana@hotmail.fr>
2023-01-15 17:37:22 +0100

    add cv page

--- src/components/0-header/NavBar.jsx
+++ src/components/0-header/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavButton from "./NavButton";
 import PageTemplate from "../1-body/content/PageTemplate";
+import Page_CV from "../1-body/content/Page_CV";
 import Page_VideoGames from "../1-body/content/Page_VideoGames";
 import Page_Books from "../1-body/content/Page_Books";
 import "./NavBar.css";
@@ -9,10 +10,10 @@ function NavBar() {
   return (
     <header>
       <div className="navbrand">
-        <NavButton link={<PageTemplate />} label="Gabriel Mattana" />
+        <NavButton link={<Page_CV />} label="Gabriel Mattana" />
       </div>
       <div className="navbar">
-        <NavButton link={<PageTemplate />} label="Projects" />
+        <NavButton link={<Page_CV />} label="Curriculum vitae" />
         <NavButton link={<Page_VideoGames/>} label="Video games" />
         <NavButton link={<Page_Books/>} label="Books" />
         <NavButton link={<PageTemplate />} label="Contact" />
---
+++ src/components/1-body/content/Page_CV.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Page_CV.css"
+
+export default function Page_CV()
+{
+    return(
+        <div className="timeline">
+        <div>skills</div>
+        <div>Experience</div>
+        <div>Journey</div>
+        </div>
+    );
+}
