2c8e7c4c34bfbe65140ab175c05f1ab0e4387be4
gabLinux <thekrelcompany@gmail.com>
2023-05-31 09:31:17 +0200

    minitaures fix font issue when resize

--- src/components/1-body/content/ContactForm.jsx
+++ src/components/1-body/content/ContactForm.jsx
@@ -7,10 +7,10 @@ export default function ContactForm() {
       <div className="form-container">
         <form className="flex-col">
           <label>Your name / company</label>
-          <input type="text" required="true"/>
+          <input type="text" required={true}/>
           <br/>
           <label> E-mail</label>
-          <input type="email" required="true"></input>
+          <input type="email" required={true}></input>
           <br/>
           <label> Message</label>
           <textarea rows="15"></textarea>
