e41a4c9d8b92e1946f9434f885861a778f899344
gabLinux <thekrelcompany@gmail.com>
2023-06-02 12:39:56 +0200

    miniature grow and shrink by animation

--- src/components/1-body/displayers/Miniature.jsx
+++ src/components/1-body/displayers/Miniature.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
-export default function Miniature({props}) {
+export default function Miniature({ props }) {
+  let miniature;
+
+  function shrinkMiniature() {
+    miniature = document.getElementById(props.title);
+    miniature.style.animation = "shrink 0.75s forwards";
+  }
+
+  function restoreMiniature() {
+    miniature.style.animation = "grow 0.5s forwards";
+  }
 
   return (
-    <div className="miniature">
-      <img src={props.img} />
-      <div className="details">
-        <p className="Title">{props.title}</p>
-        <p className="Year">{props.year}</p>
+    <div
+      className="miniature-container"
+      onMouseEnter={shrinkMiniature}
+      onMouseLeave={restoreMiniature}
+    >
+      <div id={props.title} className="miniature">
+        <img src={props.img} />
+        <div className="details">
+          <p className="Title">{props.title}</p>
+          <p className="Year">{props.year}</p>
+        </div>
       </div>
     </div>
   );
