294af8493f61f609c89d4217f78d4b4b3cbcdee3
gabLinux <thekrelcompany@gmail.com>
2023-06-05 17:01:58 +0200

    add product info dialog

--- src/components/1-body/content/data.js
+++ src/components/1-body/content/data.js
@@ -1,23 +1,100 @@
 class Data{
-    constructor(title, url, year)
+    constructor(title, url, year, description)
     {
         this.title = title;
         this.img = process.env.PUBLIC_URL + "/images/" + url;
         this.year = year;
+        this.description = description;
     }
 }
 
+function setImgUrl(url){
+    return process.env.PUBLIC_URL + "/images/" + url;
+}
+
 // VIDEO GAMES
-var bluePeace = new Data("Blue peace", "/games/blue_peace.jpg", "2022");
-var fallenStar = new Data("Fallen star", "/games/fallen_star.jpg", "2022");
-var handleTheCrate = new Data("Handle the crate", "/games/handle_the_crate.jpg", "2022");
-var clockTower = new Data("Clock tower", "/games/clock_tower.jpg", "2022");
-var marryMe = new Data("Marry me", "/games/marry_me.jpg", "2021");
+// var bluePeace = new Data("Blue peace", "/games/blue_peace.jpg", "2022",
+// "BluePeace is a visual naovle" +
+//  "visual novel");
+ var bluePeace = {
+    title: "Blue Peace",
+    img: setImgUrl("/games/blue_peace.jpg"),
+    year: "2022",
+    engine: "Godot",
+    genre: "narrative game",
+    type: "visual novel",
+    team: "1 audio, 2 artists, 2 programers",
+    role: "gameplay programer, game designer, writer, UI programer",
+    description:"Blue peace is a visual novel in which the player plays as a student" +
+    "going to their grand parent's for summer break.",
+    videosrc:"https://www.youtube.com/embed/1d7G5BOXv-U",
+ }
+
+var handleTheCrate = {
+    title: "Handle the crate",
+    img: setImgUrl("/games/handle_the_crate.jpg"),
+    year: "2022",
+    engine: "Godot",
+    genre: "puzzle game",
+    type: "sokoban",
+    team: "1 artist 4 programers",
+    role: "programer, level designer",
+    tasks: "shaders, tutorial, highscores, levels",
+    description:"Handle the crate is a puzzle which revisits the classic rules of sokoban:"+
+    "instead of only pushing crates, you can also pull them thanks to magnetic handles."+
+    "But beware! Once stuck, the handles can't be removed.",
+    videosrc: "https://www.youtube.com/embed/T9KZ6P6gASs",
+    gameurl: "https://teamcannelle.itch.io/handle-the-crate",
+ }
+
+var fallenStar= {
+    title: "Fallen Star",
+    img: setImgUrl("/games/fallen_star.jpg"),
+    year: "2022",
+    engine: "Unity",
+    genre: "narrative game",
+    type: "card game",
+    team: "solo development",
+    role: "gameplay programer, game designer, UI programer",
+    description:"A narrative cards game taking place in a Lovecraft world."+
+    "You're the king and you must make decisions to protect your realm from ruin."+
+    "Be careful, though. Your choice may affect the stability of the kingdom, and if it falls to zero, that's game over.",
+    videosrc:"https://www.youtube.com/embed/Hu0iEJKOlLE",
+}
+
+var clockTower= {
+    title: "Clock tower",
+    img: setImgUrl("/games/clock_tower.jpg"),
+    year: "2022",
+    engine: "Adobe animate",
+    genre: "action",
+    type: "shoot them up",
+    team: "1 audio artist, 1 programer",
+    role: "gameplay programer, game designer, UI programer",
+    description:"A shoot them'up with simple shapes to boost creativity." +
+    "Fly through the level to grab upgrades, dodge traps, actionate mechanism and defeat the boos",
+    videosrc:"https://www.youtube.com/embed/RcnigeDOSjs",
+}
+
+var marryMe= {
+    title: "Marry me",
+    img: setImgUrl("/games/marry_me.jpg"),
+    year: "2021",
+    engine: "Unity",
+    genre: "action",
+    type: "Topview shooter",
+    team: "2 programmers",
+    role: "gameplay programer, artist, game designer, UI programer",
+    description:"Made in 48h for the GMTK Game Jam 2021, on the theme 'joined together'."+
+    "The player have to dodge the bride and her henchmen through this topviewshooter, or they will get married.",
+    videosrc:"https://www.youtube.com/embed/R2Jisf9-CPA",
+    gameurl:"https://krel113.itch.io/marry-me",
+}
 
 var videoGames = {
     title: "Video games",
     id:"videogames",
-    data: [bluePeace, fallenStar, handleTheCrate, clockTower, marryMe]
+    data: [bluePeace, handleTheCrate, fallenStar, clockTower, marryMe]
 };
 
 // BOOKS
--- src/components/1-body/displayers/ProductInfo.jsx
+++ src/components/1-body/displayers/ProductInfo.jsx
@@ -3,12 +3,33 @@ import Miniature from "./Miniature";
 
 export default function ProductInfo({productData})
 {
+
+
+ var btnPlayGame = undefined;
+ if(productData.gameurl)
+ {
+    btnPlayGame = <a className="playgame" href={productData.gameurl} target="_blank">Play the game on itch.io</a>
+ } 
+
+
+
+
+
     return(
         <React.Fragment>
         <Miniature data={productData}/>
         <div className="product-description">
           <h2>{productData.title}</h2>
+          <p>Engine: {productData.engine}</p>
+          <p>Team: {productData.team}</p>
+          <p>Role: {productData.role}</p>
+          <p>Description: {productData.description}</p>
+          {btnPlayGame}        
         </div>
+        <div className="embededvideo">
+            <iframe width="802" height="451" src={productData.videosrc} title={productData.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        
       </React.Fragment>
     );
 }
