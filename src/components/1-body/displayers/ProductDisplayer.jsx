b8907452ce95316a5bfc1257816e968662133923
gabLinux <thekrelcompany@gmail.com>
2023-06-05 14:35:13 +0200

    productdisplayer add productdescription

--- src/components/1-body/displayers/ProductDisplayer.jsx
+++ src/components/1-body/displayers/ProductDisplayer.jsx
@@ -1,59 +1,70 @@
 import React, { useRef, useState } from "react";
 import Miniature from "./Miniature";
 
-const FADEIN = "fadeout 0.3s forwards"
+const FADEIN = "fadeout 0.3s forwards";
 
-export default function ProductDisplayer({data, sectionid}) {
+export default function ProductDisplayer({ data, sectionid }) {
   var keyNb = 0;
   let displayerid = "displayer" + sectionid;
- 
+
   var miniatures = data.map((item) => (
-    <Miniature data={item} key={keyNb++} popupCallback={displayInfoOnProduct}></Miniature>
+    <Miniature
+      data={item}
+      key={keyNb++}
+      popupCallback={displayInfoOnProduct}
+    ></Miniature>
   ));
 
-  let [stateContent, updateContent] = useState(miniatures)
+  let [stateContent, updateContent] = useState(miniatures);
 
-  function displayInfoOnProduct(miniatureSelected)
-  {
-    let displayer = document.getElementById(displayerid)
-    let children = displayer.childNodes
+  function displayInfoOnProduct(productData) {
+    let displayer = document.getElementById(displayerid);
+    let children = displayer.childNodes;
     let selection = 0;
-    
+
     for (let i = 0; i < children.length; i++) {
-      var miniatureContainer = children[i]
-      if(data[i].title != miniatureSelected.title)
-      {
-        miniatureContainer.classList.add("fadeout")
-      }
-      else
-      {
+      var miniatureContainer = children[i];
+      if (data[i].title != productData.title) {
+        miniatureContainer.classList.add("fadeout");
+      } else {
         selection = miniatureContainer.children[0];
-        selection.classList.remove("shrink-anim", "miniature-selected")
+        selection.classList.remove("shrink-anim", "miniature-selected");
       }
     }
 
-    let root = document.documentElement
-    let y =  displayer.offsetTop - selection.offsetTop;
+    let root = document.documentElement;
+    let y = displayer.offsetTop - selection.offsetTop;
     // console.log(y)
     let x = displayer.offsetLeft - selection.offsetLeft;
     // console.log(x)
     root.style.setProperty("--posX", x + "px");
     root.style.setProperty("--posY", y + "px");
 
-    console.log()
+    console.log();
 
+    let delay = 500;
     setTimeout(() => {
       selection.classList.add("toFirstCell");
-    }, 500);
+    }, delay);
 
+    let newContent = (
+      <React.Fragment>
+        <Miniature data={productData}/>
+        <div className="product-description" style={{minHeight:displayer.offsetHeight}}>
+          <h2>{productData.title}</h2>
+        </div>
+      </React.Fragment>
+      
+    );
 
-
-
+    setTimeout(() => {
+      updateContent(newContent);
+    }, delay + 400);
   }
 
   return (
     <React.Fragment>
-      <div id={displayerid} className="miniature-displayer">
+      <div id={displayerid} className="productdisplayer">
         {stateContent}
       </div>
     </React.Fragment>
